1218d14347e4dac41263f5389869ab9306631019
yeetesh <yeetesh1997@gmail.com>
2017-11-11 14:26:13 +0530

    Added all databases

--- server.py
+++ server.py
@@ -15,8 +15,7 @@ def index():
         return redirect(url_for('dashboard'))
     else:
         return redirect(url_for('sign_in'))
-
-
+# /<name>/<hello>
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -25,8 +24,6 @@ def register():
         username = (request.form['username']).lower().strip()
         password = (request.form['password'])
         is_valid_username = db.add_user(username, password)
-        for key in is_valid_username:
-            print key, is_valid_username[key]
         if is_valid_username.has_key('success'):
             flash(is_valid_username['success'] + '. Sign in now.', 'register_success')
             return redirect(url_for("sign_in"))
