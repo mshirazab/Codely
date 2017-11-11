1e58704f2669b7dde79bec6f671d9f00a19550dc
mshirazab <mshirazab@gmail.com>
2017-11-11 11:38:17 +0530

    Made some changes

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from database import Database as db
-from flask import Flask, session, request, flash, redirect, url_for
+from utils.database import Database as db
+from flask import Flask, session, request, flash, redirect, url_for, session
 # , redirect, url_for, session, request
 from flask import render_template
 # , abort
+from utils.authentication import authenticated_resource
 import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -10,7 +11,10 @@ app.secret_key = os.urandom(24)
 
 @app.route('/', methods=['GET'])
 def index():
-    return redirect('/sign_in')
+    if 'user' in session:
+        return redirect(url_for('dashboard'))
+    else:
+        return redirect(url_for('sign_in'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -27,23 +31,39 @@ def register():
             flash(is_valid_username['success'] + '. Sign in now.', 'register_success')
             return redirect(url_for("sign_in"))
         else:
-            flash('Username is already taken, MF', 'register_error')
+            flash('Username is already taken', 'register_error')
             return redirect(url_for("register"))
 
 @app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
     if request.method == 'GET':
+        if 'user' in session:
+            return redirect(url_for('dashboard'))
         return render_template('./sign_in.html', logged_in=False)
     if request.method == 'POST':
         username = (request.form['username']).lower().strip()
         password = (request.form['password'])
         can_login = db.check_can_login(username, password)
         if can_login:
+            session['user'] = username
             return redirect(url_for("index"))
         else:
-            flash(u'Invalid username or password', 'sign_in_error')
+            flash('Invalid username or password', 'sign_in_error')
             return redirect(url_for("sign_in"))
 
+
+@app.route('/dashboard', methods=['GET'])
+@authenticated_resource
+def  dashboard():
+    if request.method == 'GET':
+        return render_template('./dashboard.html')
+
+@app.route('/logout', methods=['GET'])
+@authenticated_resource
+def logout():
+    if request.method == 'GET':
+        session.pop('user', None)
+        return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
---
+++ utils/authentication.py
@@ -0,0 +1,12 @@
+from flask import redirect, session, url_for
+from functools import wraps
+
+def authenticated_resource(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'user' in session:
+            return f(*args, **kwargs)
+
+        return redirect(url_for('sign_in'))
+
+    return decorated
