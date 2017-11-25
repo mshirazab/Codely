9133cc432a6ab58a6095107b36cab84bc02eaae2
yeetesh <yeetesh1997@gmail.com>
2017-11-26 04:03:42 +0530

    Check server.py

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from flask import Flask, session, request, redirect, url_for
 from flask import render_template, abort
 import os
 import magic
+from time import gmtime, strftime
 
 mime = magic.Magic(mime=True)
 app = Flask(__name__)
@@ -113,6 +114,97 @@ def add_commit(path):
     rh.add_commit(path, files)
     return path
 
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#database functions
+
+#I think all this should rather be in database.py but ill add it here for now
+
+#add data in repositories table
+def db_add_repositories(repo_name,username):
+    _db_ = pymysql.connect(host="localhost", user="root",passwd="sudeep", db="codely")
+    _cursor_ = _db_.cursor()
+    try:
+        query = "insert into repositories(repo_name, owner) values(\"%s\", \"%s\");" % (repo_name, username)
+        _cursor_.execute(query)
+        _db_.commit()
+        return {"success": "Successfully added %s" % (repo_name)}
+    except:
+                return "Repository addition failed"
+
+#add data in collaborators
+def db_add_collaborators(repo_id ,username):
+    _db_ = pymysql.connect(host="localhost", user="root",passwd="sudeep", db="codely")
+    _cursor_ = _db_.cursor()
+    
+    #this check can be avoid if we give add collaborator option from users who are only present in the db. Has to be implemented in the front end.
+    user_exists = db.check_valid_username(username)
+    if user_exists is False:
+        return "This user is not registered and hence can not be a collaborator"
+    try:
+        query = "insert into collaborators values(%d, \"%s\");" % (repo_id, username)
+        _cursor_.execute(query)
+        _db_.commit()
+        return {"success": "Successfully added %s" % (username)}
+    except:
+        return "Collaborator addition failed"
+
+#add data in tags
+def db_add_tags(repo_id,tag):
+    _db_ = pymysql.connect(host="localhost", user="root",passwd="sudeep", db="codely")
+    _cursor_ = _db_.cursor()
+    try:
+        query = "insert into tags values(%d, \"%s\");" % (repo_id, tag)
+        _cursor_.execute(query)
+        _db_.commit()
+        return {"success": "Successfully added %d to %s" % (tag,repo_id)}
+    except:
+        return "Tag addition failed"
+
+#adding data in commit
+def db_add_commit(username,repo_id):
+    _db_ = pymysql.connect(host="localhost", user="root",passwd="sudeep", db="codely")
+    _cursor_ = _db_.cursor()
+    try:
+        current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        query = "insert into commits values(\"%s\",%d,\"%s\");" % (username,repo_id,current_time) #passing time as string to the database, verify its working.
+        _cursor_.execute(query)
+        _db_.commit()
+        return "Successfully committed"
+    except:
+        return "Commit failed"
+    
+#get data from repositories
+def db_get_repositories(username):
+    _db_ = pymysql.connect(host="localhost", user="root",passwd="sudeep", db="codely")
+    _cursor_ = _db_.cursor()
+    query = "select repo_name from repositories where owner=\"%s\"" % (username))
+    _cursor_.execute(query)
+    return _cursor_.fetchall()
+
+#get data from collaborators
+def db_get_collaborators(repo_id):
+    _db_ = pymysql.connect(host="localhost", user="root",passwd="sudeep", db="codely")
+    _cursor_ = _db_.cursor()
+    query = "select username from collaborators where repo_id=%d" % (repo_id))
+    _cursor_.execute(query)
+    return _cursor_.fetchall()
+
+#get data from commits
+def db_get_commits(repo_id):
+    _db_ = pymysql.connect(host="localhost", user="root",passwd="sudeep", db="codely")
+    _cursor_ = _db_.cursor()
+    query = "select username,commit_time from repositories where repo_id=%d" % (repo_id))
+    _cursor_.execute(query)
+    return _cursor_.fetchall()
+
+#get data from repositories
+def db_get_tags(repo_id):
+    _db_ = pymysql.connect(host="localhost", user="root",passwd="sudeep", db="codely")
+    _cursor_ = _db_.cursor()
+    query = "select topic from tags where repo_id=%d" % (repo_id))
+    _cursor_.execute(query)
+    return _cursor_.fetchall()
 
 if __name__ == "__main__":
     app.run(port=5000, threaded=True, debug=True, host='0.0.0.0')
